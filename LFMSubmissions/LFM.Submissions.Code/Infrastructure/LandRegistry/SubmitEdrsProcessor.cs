578c779c758ddd929a30546dbc10b945a256fabe
ClaireCummings <claire.cummings@advancedcomputersoftware.com>
2013-10-16 17:17:43 +0100

    SubmitEdrs starts a saga

--- LFMSubmissions/LFM.Submissions.Code/Infrastructure/LandRegistry/SubmitEdrsProcessor.cs
+++ LFMSubmissions/LFM.Submissions.Code/Infrastructure/LandRegistry/SubmitEdrsProcessor.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using NServiceBus;
 using LFMSubmissions.InternalMessages.LandRegistry;
+using NServiceBus.Saga;
 
 
 namespace LFMSubmissions.LandRegistry
 {
-    public partial class SubmitEdrsProcessor : IHandleMessages<SubmitEdrs>
+    public partial class SubmitEdrsProcessor : Saga<SubmitEdrsProcessorSagaData>, IAmStartedByMessages<SubmitEdrs>
     {
 		
 		public void Handle(SubmitEdrs message)
@@ -14,8 +15,14 @@ namespace LFMSubmissions.LandRegistry
 		}
 
 		partial void HandleImplementation(SubmitEdrs message);
+     }
 
-		public IBus Bus { get; set; }
+     public partial class SubmitEdrsProcessorSagaData : IContainSagaData
+     {
+           public virtual Guid Id { get; set; }
+           public virtual string Originator { get; set; }
+           public virtual string OriginalMessageId { get; set; }
+
 
     }
 }
